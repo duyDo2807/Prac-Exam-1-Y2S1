c650b5bfe949e3530d1ee14d88eceded31dd080a
Duc Anh Duy DO <a1881078@adelaide.edu.au>
2024-03-22 17:00:09 +1030

    Exam

--- r1.cpp
+++ r1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <cmath>
 
-int sumPower(int n, int acc = 0) {
+int sum_of_powers(int n, int acc = 0) {
     if (n == 0) {
         return acc;
     } else {
-    return sumPower(n - 1, pow(n, n) + acc);
+    return sum_of_powers(n - 1, pow(n, n) + acc);
     }
 }
